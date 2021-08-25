06f9616fee5e98f18b169f3c60b1f6aa48629648
Mariana Saraiva <marianascm8@gmail.com>
2021-08-25 15:36:59 -0300

    Iniciando atividades dia 4.1

---
+++ fundamentos/bloco-4-introducao_a_javascript_e_logica_programacao/exercicio4-1/scripts.js
@@ -0,0 +1,10 @@
+//1. variáveis
+
+const myName = "Mariana";
+let birthCity = "Belo Horizonte";
+let birthYear = 1985;
+
+birthYear = 2030;
+birthCity = "Natal";
+
+console.log(myName + " " + birthCity + " " + birthYear);
