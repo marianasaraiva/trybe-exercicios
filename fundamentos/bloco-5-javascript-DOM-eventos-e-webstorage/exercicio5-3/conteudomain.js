fdd17f7142e61cb154993cba0eaf64c39b711248
Mariana Saraiva <marianascm8@gmail.com>
2021-09-08 11:10:08 -0300

    Exercicios conteudo

--- fundamentos/bloco-5-javascript-DOM-eventos-e-webstorage/exercicio5-3/conteudomain.js
+++ fundamentos/bloco-5-javascript-DOM-eventos-e-webstorage/exercicio5-3/conteudomain.js
@@ -8,17 +8,43 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 1. Copie esse arquivo e edite apenas ele;
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento selecionado;
-// 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+
+function addClass(event) {
+  // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso? Usando o remove.
+  const classTech = document.querySelector('.tech');
+  classTech.classList.remove('tech');
+  event.target.classList.add('tech');
+  input.value = '';
+}
+
+firstLi.addEventListener('click', addClass);
+secondLi.addEventListener('click', addClass);
+thirdLi.addEventListener('click', addClass);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
+input.addEventListener('input', function (event) {
+  const classTech = document.querySelector('.tech');
+  classTech.innerText = event.target.value;
+});
+
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portifólio?
+myWebpage.addEventListener('dblclick', function() {
+  window.location.replace('https://marianasaraiva.github.io/');
+});
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+myWebpage.addEventListener('mouseover', function(event) {
+  event.target.style.color = 'green';
+});
+
+myWebpage.addEventListener('mouseout', function(event){
+  event.target.style.color = 'white';
+})
 
 // Segue abaixo um exemplo do uso de event.target:
 
