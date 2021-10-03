9e86abdf697c59c85702974881fff0799cabb476
Mariana Saraiva <marianascm8@gmail.com>
2021-10-02 21:32:22 -0300

    Vamos praticar exercicio 01

---
+++ fundamentos/bloco-8-higher-order-functions-javascript-es6/exercicio8-1/vamosPraticar1.js
@@ -0,0 +1,17 @@
+// 1 - Crie uma função que retorne um objeto no formato { nomeCompleto, email } representando uma nova pessoa contratada. Passe sua função como parâmetro da HOF newEmployees para criar cada pessoa contratada em seu respectivo id . A sua função deve receber como parâmetro o nome completo da pessoa funcionária e a partir dele gerar automaticamente um email no formato nome_da_pessoa@trybe.com .
+const contratado = (nomeCompleto) => {
+  let nome = nomeCompleto.toLowerCase().split(' ');
+  return { nome: nomeCompleto, email: `${nome.join('_')}@trybe.com`}
+}
+// console.log(contratado('Mariana Saraiva Cirino de Moura'));
+
+const newEmployees = (func) => {
+  const employees = {
+    id1: contratado('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
+    id2: contratado('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
+    id3: contratado('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
+  }
+  return employees;
+};
+
+console.log(newEmployees());
