3f80a9cdcc65fd37ca001424b13d2578555f214f
Mariana Saraiva <marianascm8@gmail.com>
2021-10-07 12:20:17 -0300

    Vamos praticar exercicio 2

--- fundamentos/bloco-8-higher-order-functions-javascript-es6/exercicio8-4/vamosPraticar1.js
+++ fundamentos/bloco-8-higher-order-functions-javascript-es6/exercicio8-4/vamosPraticar1.js
@@ -8,6 +8,7 @@ const arrays = [
 
 function flatten() {
   // concat() => Arrays ou valores para concatenar (unir) ao array retornado.
+  // return arrays.reduce((acc, value) => acc.concat(value)); // outra forma de fazer que funciona.
   const newArr = arrays.reduce((acumulador, array) => acumulador.concat(array), []);
   return newArr;
   // return arrays.reduce((acumulador, array) => acumulador + array ); // 1,2,3true4,5,6 (retorna errado, como se fosse uma string)
