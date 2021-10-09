be978b98394f8df9b8f597eff6a58ef60bfd5ac8
Mariana Saraiva <marianascm8@gmail.com>
2021-10-09 18:33:30 -0300

    Requisito 3

--- fundamentos/bloco-8-higher-order-functions-javascript-es6/projetoZooFunctions/sd-016-b-project-zoo-functions-master/src/getEmployeeByName.js
+++ fundamentos/bloco-8-higher-order-functions-javascript-es6/projetoZooFunctions/sd-016-b-project-zoo-functions-master/src/getEmployeeByName.js
@@ -1,7 +1,21 @@
 const data = require('../data/zoo_data');
 
+// usei a HOF find para encontrar o elemento de acordo com firstName e lastName do colaborador, uma vez que ele retornou um objeto.
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const { employees } = data;
+  if (employeeName === undefined) return { };
+  return employees.find((e) => e.firstName === employeeName || e.lastName === employeeName);
 }
 
+// console.log(getEmployeeByName('Emery'));
+// console.log(getEmployeeByName('Wishart'));
+// console.log(getEmployeeByName());
 module.exports = getEmployeeByName;
+
+// Esta função é responsável pela busca das pessoas colaboradoras através do primeiro ou do último nome delas
+
+// O que será avaliado
+
+// Sem parâmetros, retorna um objeto vazio
+// Quando provido o primeiro nome do funcionário, retorna o objeto do funcionário
+// Quando provido o último nome do funcionário, retorna o objeto do funcionário
