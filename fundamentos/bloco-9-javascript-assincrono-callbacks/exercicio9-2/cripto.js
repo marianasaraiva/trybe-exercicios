85e8ee5de3060901c485c6863e2637309727d64e
Mariana Saraiva <marianascm8@gmail.com>
2021-10-19 11:44:21 -0300

    Monitoria

--- fundamentos/bloco-9-javascript-assincrono-callbacks/exercicio9-2/cripto.js
+++ fundamentos/bloco-9-javascript-assincrono-callbacks/exercicio9-2/cripto.js
@@ -1,29 +1,10 @@
-// const URL = 'https://api.coincap.io/v2/assets';
-
-// const criptoMoedas = async () => {
-//   const moeda = await fetch(URL)
-//     .then((response) => response.json())
-//     .then((data) => console.log(data))
-//     .catch((error) => console.log('Seu erro é:', error))
-//   return moeda;
-// };
-
-// const moedalista = async () => {
-//   const moeda = await criptoMoedas();
-
-//   const moedaUL = document.getElementById('lista');
-
-//   moeda.forEach((item) => {
-//     const li = document.createElement('li');
-//     const precoDolar = Number(item.priceUsd);
-//     li.innerText = `${item.name} (${item.symbol}): ${precoDolar.toFixed(2)}`;
-//     moedaUL.appendChild(li);
-//   });
-// }
-
-// moedalista()
-// window.onload = () => moedalista();
-
+const criptoMoedas = (url) => {
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => append(data))
+  .catch ((error) => console.log('Seu erro é:', error))
+};
+criptoMoedas('https://api.coincap.io/v2/assets');
 
 const append = ({ data }) => {
   const ul = document.querySelector('#lista');
@@ -36,13 +17,3 @@ const append = ({ data }) => {
   });
 }
 
-const criptoMoedas = (url) => {
-  fetch(url)
-  .then((response) => response.json())
-    .then((data) => append(data))
-    // console.log(data);
-    .catch ((error) => console.log('Seu erro é:', error))
-    // return teste;
-  };
-  
-criptoMoedas('https://api.coincap.io/v2/assets');
