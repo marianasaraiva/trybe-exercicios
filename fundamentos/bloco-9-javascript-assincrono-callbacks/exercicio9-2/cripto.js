5f0248677bee5edd6fb2e33ec0788cb51c8b476b
Mariana Saraiva <marianascm8@gmail.com>
2021-10-19 10:38:53 -0300

    Vamos Praticar finalizado

--- fundamentos/bloco-9-javascript-assincrono-callbacks/exercicio9-2/cripto.js
+++ fundamentos/bloco-9-javascript-assincrono-callbacks/exercicio9-2/cripto.js
@@ -1,22 +1,48 @@
-// https://api.coincap.io/v2/assets
-// const fetch = require('node-fetch');
+// const URL = 'https://api.coincap.io/v2/assets';
 
-const append = (data) => {
-  const ul = document.querySelector('ul');
-  const li = document.createElement('li');
-  innerText = data[0].name;
-  ul.appendChild(li);
+// const criptoMoedas = async () => {
+//   const moeda = await fetch(URL)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data))
+//     .catch((error) => console.log('Seu erro é:', error))
+//   return moeda;
+// };
+
+// const moedalista = async () => {
+//   const moeda = await criptoMoedas();
+
+//   const moedaUL = document.getElementById('lista');
+
+//   moeda.forEach((item) => {
+//     const li = document.createElement('li');
+//     const precoDolar = Number(item.priceUsd);
+//     li.innerText = `${item.name} (${item.symbol}): ${precoDolar.toFixed(2)}`;
+//     moedaUL.appendChild(li);
+//   });
+// }
+
+// moedalista()
+// window.onload = () => moedalista();
+
+
+const append = ({ data }) => {
+  const ul = document.querySelector('#lista');
+  data.filter((filter, index) => index < 10).forEach((item) => {
+    const li = document.createElement('li');
+    // const precoDolar = Number(item.priceUsd);
+    // li.innerText = `${item.name} (${item.symbol}): ${precoDolar.toFixed(2)}`;
+    li.innerText = `${item.name} (${item.symbol}): ${Number(item.priceUsd).toFixed(2)}`;
+    ul.appendChild(li);
+  });
 }
 
 const criptoMoedas = (url) => {
   fetch(url)
-    .then((response) => response.json())
+  .then((response) => response.json())
     .then((data) => append(data))
+    // console.log(data);
     .catch ((error) => console.log('Seu erro é:', error))
-};
-
-async function apiCripto() {
-  const data = await criptoMoedas('https://api.coincap.io/v2/assets');
-  return data;
-}
-apiCripto();
+    // return teste;
+  };
+  
+criptoMoedas('https://api.coincap.io/v2/assets');
