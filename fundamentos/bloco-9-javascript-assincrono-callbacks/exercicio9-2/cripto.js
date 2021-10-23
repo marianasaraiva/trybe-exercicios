77344ee7a012976eb63dcfdca1df70a8eef1d532
Mariana Saraiva <marianascm8@gmail.com>
2021-10-23 15:17:26 -0300

    Atualizo dia 9.2

--- fundamentos/bloco-9-javascript-assincrono-callbacks/exercicio9-2/cripto.js
+++ fundamentos/bloco-9-javascript-assincrono-callbacks/exercicio9-2/cripto.js
@@ -1,19 +1,19 @@
 const criptoMoedas = (url) => {
   fetch(url)
   .then((response) => response.json())
-  .then((data) => append(data))
+  .then((data) => (data))
   .catch ((error) => console.log('Seu erro é:', error))
 };
-criptoMoedas('https://api.coincap.io/v2/assets');
+console.log(criptoMoedas('https://api.coincap.io/v2/assets'));
 
-const append = ({ data }) => {
-  const ul = document.querySelector('#lista');
-  data.filter((filter, index) => index < 10).forEach((item) => {
-    const li = document.createElement('li');
-    // const precoDolar = Number(item.priceUsd);
-    // li.innerText = `${item.name} (${item.symbol}): ${precoDolar.toFixed(2)}`;
-    li.innerText = `${item.name} (${item.symbol}): ${Number(item.priceUsd).toFixed(2)}`;
-    ul.appendChild(li);
-  });
-}
+// const append = ({ data }) => {
+//   const ul = document.querySelector('#lista');
+//   data.filter((filter, index) => index < 10).forEach((item) => {
+//     const li = document.createElement('li');
+//     // const precoDolar = Number(item.priceUsd);
+//     // li.innerText = `${item.name} (${item.symbol}): ${precoDolar.toFixed(2)}`;
+//     li.innerText = `${item.name} (${item.symbol}): ${Number(item.priceUsd).toFixed(2)}`;
+//     ul.appendChild(li);
+//   });
+// }
 
