be9ff3d8f8c74ae89314214368d8956ccb611a21
Mariana Saraiva <marianascm8@gmail.com>
2021-12-06 18:32:04 -0300

    Commit inicial

---
+++ projetos-pessoais/jogodavelha/components/Celula.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Celula extends Component {
+  render() {
+    return (
+      <div>
+        Células
+      </div>
+    )
+  }
+}
---
+++ projetos-pessoais/jogodavelha/components/Matriz.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Matriz extends Component {
+  render() {
+    return (
+      <div>
+        Matriz
+      </div>
+    )
+  }
+}
