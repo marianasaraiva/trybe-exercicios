483e4e4798ab5b5c8b4fd017a03259c5a6e582eb
Mariana Saraiva <marianascm8@gmail.com>
2022-02-22 10:23:08 -0300

    feat: exercise finish

---
+++ back-end/bloco20-introducao-a-sql/dia20-3-filtando-dados-de-forma-especifica/exercise-fixation/exercise4.sql
@@ -0,0 +1,11 @@
+USE sakila;
+SELECT COUNT(*) FROM sakila.payment WHERE DATE(payment_date) = '2005-05-25';
+SELECT COUNT(*) FROM sakila.payment WHERE payment_date BETWEEN '2005-07-01' AND '2005-08-22';
+SELECT DATE(rental_date) AS Data,
+	YEAR(rental_date) AS Ano ,
+	MONTH(rental_date) AS Mes,
+	DAY(rental_date) AS Dia,
+	HOUR(rental_date) AS Hora,
+	MINUTE(rental_date) AS Minuto,
+	SECOND(rental_date) AS Segundo FROM rental WHERE rental_id = 10330;
+SELECT * FROM sakila.payment WHERE DATE(payment_date) = '2005-07-28' AND HOUR(payment_date) >= 22;
