61945d8d10b810db373c10c168e80f778ecc8bc2
Mariana Saraiva <marianascm8@gmail.com>
2022-03-11 12:31:00 -0300

    feat: pratice-exercise 5

--- back-end/bloco22-introducao-ao-desenvolvimento-web-com-nodejs/dia22-1-nodejs-um-motor-javascript/my-script/imc.js
+++ back-end/bloco22-introducao-ao-desenvolvimento-web-com-nodejs/dia22-1-nodejs-um-motor-javascript/my-script/imc.js
@@ -7,8 +7,19 @@ const imc = () => {
 
   console.log(`Peso: ${peso}, Altura: ${altura}`);
   const result = (peso / Math.pow(altura / 100, 2)).toFixed(2);
-  console.log(`IMC: ${result}`);
-
+  if (result < 18.5) {
+    console.log(`IMC: ${result}, Classificação: Abaixo do peso (magreza)`);
+  } else if (result <= 24.9) {
+    console.log(`IMC: ${result}, Classificação: Peso normal`);
+  } else if (result <= 29.9) {
+    console.log(`IMC: ${result}, Classificação: Acima do peso (sobrepeso)`);
+  } else if (result <= 34.9) {
+    console.log(`IMC: ${result}, Classificação: Obesidade grau I`);
+  } else if (result <= 39.9) {
+    console.log(`IMC: ${result}, Classificação: Obesidade grau II`);
+  } else {
+    console.log(`IMC: ${result}, Classificação: Obesidade graus III e IV`);
+  }
 }
 
 imc();
