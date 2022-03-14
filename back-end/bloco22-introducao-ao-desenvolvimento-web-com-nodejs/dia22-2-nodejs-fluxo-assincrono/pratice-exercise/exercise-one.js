22c35af154211267cd52a1ea5e096501086b8ad3
Mariana Saraiva <marianascm8@gmail.com>
2022-03-14 12:24:26 -0300

    fix: pratice-exercise one

--- back-end/bloco22-introducao-ao-desenvolvimento-web-com-nodejs/dia22-2-nodejs-fluxo-assincrono/pratice-exercise/exercise-one.js
+++ back-end/bloco22-introducao-ao-desenvolvimento-web-com-nodejs/dia22-2-nodejs-fluxo-assincrono/pratice-exercise/exercise-one.js
@@ -1,6 +1,6 @@
 function mathCount(num1, num2, num3) {
   const promise = new Promise((resolve, reject) => {
-    if (typeof num1 !== 'number' && typeof num2 !== 'number' && typeof num3 !== 'number') reject(new Error("Informe apenas números"));
+    if (typeof num1 !== 'number' || typeof num2 !== 'number' || typeof num3 !== 'number') reject(new Error("Informe apenas números"));
 
     const resultado = ((num1 + num2) * num3);
     if (resultado <= 50) resolve ("Valor muito baixo");
@@ -10,6 +10,14 @@ function mathCount(num1, num2, num3) {
   return promise;
 }
 
-mathCount(2, 8, 7)
-  .then(result => console.log(`sucesso: ${result}`))
+mathCount('r', 8, 7)
+  .then(result => console.log(`sucesso1: ${result}`))
+  .catch(err => console.log(`erro: ${err.message}`));
+
+  mathCount(3, 8, 7)
+  .then(result => console.log(`sucesso2: ${result}`))
+  .catch(err => console.log(`erro: ${err.message}`));
+
+mathCount(1, 2, 7)
+  .then(result => console.log(`sucesso3: ${result}`))
   .catch(err => console.log(`erro: ${err.message}`));
