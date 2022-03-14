f3a350b08d31fd12d2f4ca427f3b1f03debfa179
Mariana Saraiva <marianascm8@gmail.com>
2022-03-14 11:48:08 -0300

    feat: fixation exercise

---
+++ back-end/bloco22-introducao-ao-desenvolvimento-web-com-nodejs/dia22-2-nodejs-fluxo-assincrono/io-local/writeFile.js
@@ -0,0 +1,24 @@
+// const fs = require('fs').promises;
+
+// fs.writeFile('./meu-arquivo.txt', 'Meu textão')
+//   .then(() => {
+//     console.log('Arquivo escrito com sucesso!');
+//   })
+//   .catch((err) => {
+//     console.error(`Erro ao escrever o arquivo: ${err.message}`);
+//   });
+
+// // Exemplo 2 usando async/await
+// const fs = require('fs').promises;
+
+// async function main() {
+//   try {
+//     await fs.writeFile('./meu-arquivo.txt', 'Teste Async/await');
+//     console.log('Arquivo escrito com sucesso!');
+//   } catch (err) {
+//     console.error(`Erro ao escrever o arquivo: ${err.message}`);
+//   }
+// }
+
+// main()
+
