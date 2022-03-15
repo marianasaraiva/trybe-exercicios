31ba5433e0899bb376c68c067b25f232dd89f49e
Mariana Saraiva <marianascm8@gmail.com>
2022-03-15 15:28:25 -0300

    feat: exercise-one

---
+++ back-end/bloco22-introducao-ao-desenvolvimento-web-com-nodejs/dia22-3-mocha-chai-sinon-testes-em-back-end-e-nodejs/exercise-one/exercise.test.js
@@ -0,0 +1,37 @@
+const { expect } = require("chai");
+const numeroInformado = require('./exercise');
+
+describe('verifica numero positivo, negativo ou neutro', () => {
+  describe('verifica se N > 0', () => {
+    it('verifica se é uma string', () => {
+      const resposta = numeroInformado(5)
+      expect(resposta).to.be.a('string')
+    });
+    it('verifica se é igual a "positivo"', () => {
+      const resposta = numeroInformado(5)
+      expect(resposta).to.be.equals('positivo')
+    });    
+  });
+
+  describe('verifica se N === 0', () => {
+    it('verifica se é uma string', () => {
+      const resposta = numeroInformado(0)
+      expect(resposta).to.be.a('string')
+    });
+    it('verifica se é igual a "neutro"', () => {
+      const resposta = numeroInformado(5)
+      expect(resposta).to.be.equals('neutro')
+    });    
+  });
+
+  describe('verifica se N < 0', () => {
+    it('verifica se é uma string', () => {
+      const resposta = numeroInformado(-5)
+      expect(resposta).to.be.a('string')
+    });
+    it('verifica se é igual a "negativo"', () => {
+      const resposta = numeroInformado(5)
+      expect(resposta).to.be.equals('negativo')
+    });    
+  });
+});
