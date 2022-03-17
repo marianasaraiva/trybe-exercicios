c23adeb85e2b6e84cfc07878c0abc4c61185fe0f
Mariana Saraiva <marianascm8@gmail.com>
2022-03-17 08:59:32 -0300

    feat: exercise 03

--- back-end/bloco22-introducao-ao-desenvolvimento-web-com-nodejs/dia22-4-express-http-com-nodejs/exercise/index.js
+++ back-end/bloco22-introducao-ao-desenvolvimento-web-com-nodejs/dia22-4-express-http-com-nodejs/exercise/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use(express.json());
+app.use(bodyParser.json());
 
 
 // 01. Crie uma rota GET /ping
@@ -18,8 +19,23 @@ app.post('/hello', (req, res, next) => {
   res.status(200).json({ "message": `Hello, ${name}!` })
 });
 
+// 03. Crie uma rota POST /greetings
+// Sua rota deve receber o seguinte JSON: { "name": "<nome do usuário>", "age": <idade do usuário> } .
+// Caso a pessoa usuária tenha idade superior a 17 anos, devolva o JSON { "message": "Hello, <nome do usuário>!" } com o status code 200 - OK .
+// Caso a pessoa usuária tenha 17 anos ou menos, devolva o JSON { "message": "Unauthorized" } com o status code 401 - Unauthorized .
+app.post('/greetings', (req, res, next) => {
+  const { name, age } = req.body;
+  if( parseInt(age, 10) > 17 ) res.status(200).json({ "message": `Hello, ${name}!` });
+  return res.status(401).json({ "message": "Unauthorized" });
+});
 
 
+// Tratando o erro
+app.use((err, req, res, next) => {
+  res.status(500).send(`Messagem de erro: ${err.message}`);
+});
+
+// selecionando uma porta para ouvir a aplicação:
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
 });
