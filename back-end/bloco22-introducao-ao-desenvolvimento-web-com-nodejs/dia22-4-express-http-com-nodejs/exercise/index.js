92b0bd6c79b3be5a4e5f7a79c93c169ee7c85e30
Mariana Saraiva <marianascm8@gmail.com>
2022-03-17 08:46:51 -0300

    feat: exercise 02

--- back-end/bloco22-introducao-ao-desenvolvimento-web-com-nodejs/dia22-4-express-http-com-nodejs/exercise/index.js
+++ back-end/bloco22-introducao-ao-desenvolvimento-web-com-nodejs/dia22-4-express-http-com-nodejs/exercise/index.js
@@ -3,10 +3,23 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
+
+// 01. Crie uma rota GET /ping
+// Sua rota deve retornar o seguinte JSON: { message: 'pong' }
 app.get('/ping', (req, res, next) => {
   res.status(200).send( { message: 'pong' });
 });
 
+// 02. Crie uma rota POST /hello
+// Sua rota deve receber, no body da requisição, o seguinte JSON: { "name": "<nome do usuário>" }
+// Sua rota deve retornar o seguinte JSON: { "message": "Hello, <nome do usuário>!" } .
+app.post('/hello', (req, res, next) => {
+  const { name } = req.body;
+  res.status(200).json({ "message": `Hello, ${name}!` })
+});
+
+
+
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
-})
+});
