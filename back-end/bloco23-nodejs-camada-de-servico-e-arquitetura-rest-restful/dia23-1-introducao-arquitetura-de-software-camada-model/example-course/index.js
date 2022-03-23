4f69e69e7c8aceca044667217e01db8e04a1772a
Mariana Saraiva <marianascm8@gmail.com>
2022-03-23 08:55:18 -0300

    feat: fixation exercise 2

--- back-end/bloco23-nodejs-camada-de-servico-e-arquitetura-rest-restful/dia23-1-introducao-arquitetura-de-software-camada-model/example-course/index.js
+++ back-end/bloco23-nodejs-camada-de-servico-e-arquitetura-rest-restful/dia23-1-introducao-arquitetura-de-software-camada-model/example-course/index.js
@@ -19,6 +19,16 @@ app.get('/books', async (_req, res) => {
   res.status(200).json(books);
 });
 
+app.get('/books/:id', async (req, res) => {
+  const { id } = req.params;
+  const books = await Books.findById(id);
+  
+  if (!books) return res.status(404).json({ message: 'Not found'});
+
+  res.status(200).json(books);
+
+});
+
 app.listen(PORT, () => {
   console.log(`Ouvindo a porta ${PORT}`);
 });
--- back-end/bloco23-nodejs-camada-de-servico-e-arquitetura-rest-restful/dia23-1-introducao-arquitetura-de-software-camada-model/example-course/models/Books.js
+++ back-end/bloco23-nodejs-camada-de-servico-e-arquitetura-rest-restful/dia23-1-introducao-arquitetura-de-software-camada-model/example-course/models/Books.js
@@ -9,12 +9,26 @@ const serialize = (bookData) => ({
 
 // Busca todos os livros do banco.
 const getAll = async () => {
-  const [books] = await connection.execute(
-    'SELECT id, title, author_id FROM model_example.books;',
-  );
+  const query = 'SELECT id, title, author_id FROM model_example.books;'
+  const [books] = await connection.execute(query);
   return books.map(serialize);
 };
 
+const findById = async (authorId) => {
+  const query = 'SELECT * FROM model_example.books WHERE id = ?;'
+  const [booksId] = await connection.execute(query, [authorId]);
+  const result = await booksId.map(({ id, title, author_id}) => ({
+    id,
+    title,
+    authorId: author_id,
+  }));
+
+  if (booksId.length === 0) return null;
+
+  return result;
+}
+
 module.exports = {
   getAll,
+  findById,
 };  
