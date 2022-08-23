68d41c882a59d71c91cc36d41054a94d453281f7
Mariana Saraiva <marianascm8@gmail.com>
2022-08-23 20:29:18 -0300

    feat: pratice exercise 3

--- ciencia-da-computacao/bloco35-redes-e-raspagem-de-dados/dia35-1-arquitetura-de-redes-ferramentas-e-seguranca/exercise1.py
+++ ciencia-da-computacao/bloco35-redes-e-raspagem-de-dados/dia35-1-arquitetura-de-redes-ferramentas-e-seguranca/exercise1.py
@@ -42,3 +42,8 @@
 # 2. Faça uma nova chamada a "google.com", porém agora utilizando o parâmetro
 # -L ou --location, que serve para "seguir redirecionamentos".
 # curl -L google.com
+
+# Exercício 3: Agora utilizando o wget, pegue o conteúdo da página do site da
+#  Trybe, depois abra o arquivo HTML baixado em seu navegador.
+#  Faça o mesmo processo com outras páginas web.
+# wget betrybe.com
