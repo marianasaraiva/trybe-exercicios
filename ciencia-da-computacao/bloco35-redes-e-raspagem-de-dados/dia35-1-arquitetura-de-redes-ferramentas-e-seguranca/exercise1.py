abd21b4f6529569b339dc3a9cb59e0fd95a3020d
Mariana Saraiva <marianascm8@gmail.com>
2022-08-23 20:39:31 -0300

    feat: pratice exercise 6

--- ciencia-da-computacao/bloco35-redes-e-raspagem-de-dados/dia35-1-arquitetura-de-redes-ferramentas-e-seguranca/exercise1.py
+++ ciencia-da-computacao/bloco35-redes-e-raspagem-de-dados/dia35-1-arquitetura-de-redes-ferramentas-e-seguranca/exercise1.py
@@ -93,3 +93,12 @@ if __name__ == "__main__":
 # Após estabelecer a conexão, digite a mensagem e tecle enter para enviá-las.
 
 
+# Exercício 6: Reinicie o servidor TCP e agora faça uma requisição utilizando
+# o cURL (HTTP).
+# Perceba o que é exibido no console do server, já que não estamos utilizando
+# o HTTP nele. Perceba também que o comando cURL não recebe uma resposta HTTP
+# com sentido (um status code 200, por exemplo), de modo que ele não sabe que
+# aquela requisição chegou ao fim.
+# curl localhost:8085
+
+
