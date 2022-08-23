bc476dccd3e1b4a2b273681b8836c3058ea1a391
Mariana Saraiva <marianascm8@gmail.com>
2022-08-23 20:26:11 -0300

    feat: pratice exercise 1

---
+++ ciencia-da-computacao/bloco35-redes-e-raspagem-de-dados/dia35-1-arquitetura-de-redes-ferramentas-e-seguranca/exercise1.py
@@ -0,0 +1,30 @@
+# Exercício 1: O primeiro server que iremos utilizar é o nosso velho amigo
+# HTTP, na camada de aplicação. Você pode tanto criar um, como utilizar um dos
+# projetos ou exercícios dos módulos anteriores. A ideia é utilizarmos os
+# conhecimentos do conteúdo e a ferramenta cURL para realizarmos uma chamada
+# HTTP para ele. O projeto deve ter rotas GET e POST para que seja possível
+# enviar requisições para os endpoints e receber respostas, assim como já nos
+# acostumamos a receber via browser ou utilizando programas como o Postman.
+
+# Caso tenha dificuldades maiores, você pode utilizar o Postman para converter
+# uma requisição em cURL, é só fazer a requisição nele como você já sabe e
+# depois clicar no botão code (que fica embaixo do save) e escolher cURL.
+
+# 1. Faça uma chamada GET, utilizando o cURL.
+# curl localhost:3000
+
+# 2. Faça uma chamada POST, utilizando o cURL, passando um JSON no body da
+# requisição.
+# curl -X POST \
+#     -H 'Content-Type: application/json' \
+#     -d '{ "foo": "bar" }' \
+#     localhost:3000
+
+
+# 3. Faça uma chamada qualquer, utilizando o cURL, passando um header na
+# requisição.
+# curl --request POST \
+#     --header 'Content-Type: application/json' \
+#     --header 'Authorization: ApiKey EXAMPLE-TOKEN' \
+#     --data '{ "foo": "bar" }' \
+#     localhost:3000
